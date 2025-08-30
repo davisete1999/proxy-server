f1831cc18eb4ade21d6bd661a0617cef6ce9883e
davisete1999 <davidortizgonzalez1999@gmail.com>
2025-08-30 13:03:00 +0000

    .

--- internal/config/sessions.py
+++ internal/config/sessions.py
@@ -27,6 +27,18 @@ PROXY_SESSIONS = {
         },
         timeout=DEFAULT_SESSION_TIMEOUT,
     ),
+    "Binance": ProxySession(
+        name="Binance",
+        url="https://api.binance.com/api/v3/exchangeInfo",
+        headers={
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Language": "es-ES,es;q=0.9,en;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+        },
+        timeout=DEFAULT_SESSION_TIMEOUT,
+    ),
 }
 
 def get_headers_from_session(session_name: str) -> Dict[str, str]:
